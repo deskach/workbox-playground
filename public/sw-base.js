79478a7740b67e9906aa58856c525bdb82ab7b3f
Dennis <dzianis.kachanau@pepperstone.com>
2018-07-25 13:25:44 +1000

    experimented with caching strategies

--- public/sw-base.js
+++ public/sw-base.js
@@ -1,7 +1,23 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js")
 
 workbox.routing.registerRoute(new RegExp(/.*(?:googleapis|gstatic)\.com.*$/), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'google-fonts'
+  cacheName: 'google-fonts',
+  plugins: [
+    new workbox.expiration.Plugin({
+      // Only cache requests for a week
+      maxAgeSeconds: 7 * 24 * 60 * 60,
+      // Only cache 10 requests.
+      maxEntries: 10,
+    })
+  ]
+}))
+workbox.routing.registerRoute(
+  "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'material-css'
+  }))
+workbox.routing.registerRoute(new RegExp(/.*firebasestorage\.googleapis\.com.*$/), workbox.strategies.staleWhileRevalidate({
+  cacheName: 'post-images'
 }))
 workbox.routing.registerRoute(new RegExp('/images/'), workbox.strategies.cacheFirst({cacheName: 'image-cache'}))
 
